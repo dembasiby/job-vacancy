b1e982a60b1e13076c1746186de821b30c16d752
Demba Siby <dembasiby@gmail.com>
2017-07-29 12:32:14 +0000

    Updated page_controller test file..

--- spec/app/controllers/page_controller_spec.rb
+++ spec/app/controllers/page_controller_spec.rb
@@ -1,13 +1,25 @@
 require 'spec_helper'
 
-RSpec.describe "/page" do
-  pending "add some examples to #{__FILE__}" do
-    before do
-      get "/page"
-    end
+RSpec.describe "PageController" do
+  describe "GET #about" do
 
-    it "returns hello world" do
-      expect(last_response.body).to eq "Hello World"
+    it "renders the :about view" do
+      get '/about'
+      expect(last_response).to be_ok
+    end
+  end
+
+  describe "GET #contact" do
+    it "renders the :contact view" do
+      get '/contact'
+      expect(last_response).to be_ok
+    end
+  end
+
+  describe "GET #home" do
+    it "renders the :home view" do
+      get '/'
+      expect(last_response).to be_ok
     end
   end
 end
