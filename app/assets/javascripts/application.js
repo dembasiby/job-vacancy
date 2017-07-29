38c71f4ac08e415dc9f76ddd0488ba58bc6f59d3
Demba Siby <dembasiby@gmail.com>
2017-07-29 10:43:26 +0000

    Updated the application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,7 @@
 // Put your application scripts here
+/* app/assets/javascript/application.js */
+
+// This is a manifest file that'll be compiled into including all the files ...
+// ...
+//
+//= require_tree .
