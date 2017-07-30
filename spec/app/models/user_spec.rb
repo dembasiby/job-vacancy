f16f4f09d5187b749abd1dbf8bb1aaa66a2caa4f
Demba Siby <dembasiby@gmail.com>
2017-07-30 19:59:53 +0000

    Created a user with the factory for tests.

--- spec/app/models/user_spec.rb
+++ spec/app/models/user_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 
 RSpec.describe User do
   describe "User Model" do
-    let(:user) { User.new }
+    let(:user) { FactoryGirl.build(:user) }
+    let(:job_offer) { {title: 'Padrino Engineer', location: 'Dakar',
+      description: 'Come to this great place'} }
+
     it 'can be created' do
       expect(user).not_to be_nil
+    end
+
+    it 'fresh user should have no offers' do
+      expect(user.job_offers.size).to eq 0
+    end
+
+    it 'have job-offers' do
+      user.job_offers.build(job_offer)
+      expect(user.job_offers.size).to eq 1
     end
   end
 end
