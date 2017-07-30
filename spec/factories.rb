062b37cb824a593502a793f6817bdf820f9c8a0b
Demba Siby <dembasiby@gmail.com>
2017-07-30 20:06:23 +0000

    Created a factory for the job offer.

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,13 @@ FactoryGirl.define do
     name 'Demba Siby'
     email 'dembasiby@gmail.com'
   end
+
+  factory :job_offer do
+    title 'Padrino Engineer'
+    location 'Dakar'
+    description 'We want you ...'
+    contact 'recruiter@awsome.sn'
+    time_start '08/01/2017'
+    time_end '08/10/2017'
+  end
 end
